e34eaaf8345e1e8002791fe7a5faf05c92dc5bac
Jan Schöppach <jan.schoeppach@bestsens.de>
2018-08-13 15:19:56 +0200

    Add fake-option to test real values

--- bemos_modbus_client.cpp
+++ bemos_modbus_client.cpp
@@ -42,15 +42,18 @@ system_helper::LogManager logfile("bemos-modbus-client");
 #define ADDR_INPUT_COILS_START				0x00C0 
 #define ADDR_OUTPUT_COILS_START				0x01E0
 
+#define CONV_CURRENT 	0
+#define CONV_TEMP		1
+
 const json mb_register_map = {
-	{{"parameter name", "pump_casing/sealed_medium/pressure"}, 						{"address offset", 19}},
-	{{"parameter name", "sealing_chamber/barrier_fluid/pressure"}, 					{"address offset", 20}},
-	{{"parameter name", "sealing_chamber/barrier_fluid/flow"}, 						{"address offset", 21}},
-	{{"parameter name", "tank/barrier_fluid/level_barrier_fluid"}, 					{"address offset", 22}},
-	{{"parameter name", "sealing_chamber/barrier_fluid/temp_inlet"}, 				{"address offset", 43}},
-	{{"parameter name", "sealing_chamber/barrier_fluid/temp_outlet"}, 				{"address offset", 44}},
-	{{"parameter name", "water_cooler/cooling_water_barrier_system/temp_inlet"},	{"address offset", 45}},
-	{{"parameter name", "water_cooler/cooling_water_barrier_system/temp_outlet"}, 	{"address offset", 46}}
+	{{"parameter name", "pump_casing/sealed_medium/pressure"}, 						{"address offset", 19},	{"conversion", CONV_CURRENT}},
+	{{"parameter name", "sealing_chamber/barrier_fluid/pressure"}, 					{"address offset", 20},	{"conversion", CONV_CURRENT}},
+	{{"parameter name", "sealing_chamber/barrier_fluid/flow"}, 						{"address offset", 21},	{"conversion", CONV_CURRENT}},
+	{{"parameter name", "tank/barrier_fluid/level_barrier_fluid"}, 					{"address offset", 22},	{"conversion", CONV_CURRENT}},
+	{{"parameter name", "sealing_chamber/barrier_fluid/temp_inlet"}, 				{"address offset", 43},	{"conversion", CONV_TEMP}},
+	{{"parameter name", "sealing_chamber/barrier_fluid/temp_outlet"}, 				{"address offset", 44},	{"conversion", CONV_TEMP}},
+	{{"parameter name", "water_cooler/cooling_water_barrier_system/temp_inlet"},	{"address offset", 45},	{"conversion", CONV_TEMP}},
+	{{"parameter name", "water_cooler/cooling_water_barrier_system/temp_outlet"}, 	{"address offset", 46},	{"conversion", CONV_TEMP}}
 };
 
 void check_errorcode(uint16_t offset, int16_t val) {
@@ -80,8 +83,15 @@ float getFloat(const uint16_t* start, uint16_t offset) {
 	return val;
 }
 
+template<typename _NumericType = uint16_t>
+_NumericType interpolate(double from, double to, double value, _NumericType int_from, _NumericType int_to) {
+	return int_from * (1 - (value - from) / (to - from)) + int_to * ((value - from) / (to - from));
+}
+
+
 int main(int argc, char **argv){
 	bool daemon = false;
+	bool fake = false;
 
 	logfile.setMaxLogLevel(LOG_INFO);
 
@@ -112,6 +122,7 @@ int main(int argc, char **argv){
 			("suppress_syslog", "do not output syslog messages to stdout")
 			("mb_tcp_client", "modbus tcp server", cxxopts::value<std::string>(mb_tcp_target)->default_value(mb_tcp_target))
 			("mb_tcp_port", "modbus tcp server port", cxxopts::value<int>(mb_tcp_port))
+			("fake", "fake data", cxxopts::value<bool>(fake))
 		;
 
 		try {
@@ -186,10 +197,12 @@ int main(int argc, char **argv){
 		return EXIT_FAILURE;
 	}
 
-	if(modbus_connect(ctx) == -1) {
-		logfile.write(LOG_CRIT, "failed to connect to modbus client, exiting");
-		modbus_free(ctx);
-		return EXIT_FAILURE;
+	if(!fake) {
+		if(modbus_connect(ctx) == -1) {
+			logfile.write(LOG_CRIT, "failed to connect to modbus client, exiting");
+			modbus_free(ctx);
+			return EXIT_FAILURE;
+		}
 	}
 
 	/* Deamonize */
@@ -210,13 +223,77 @@ int main(int argc, char **argv){
 
 	bestsens::system_helper::systemd::ready();
 
+	auto temperature_to_register_value = [&](double temperature) -> uint16_t {
+		short temp = static_cast<short>(temperature * 10);
+		return uint16_t(temp);
+	};
+
+	auto register_value_to_temperature = [&](uint16_t value) -> double {
+		return double(value) / 10;
+	};
+
+	auto convert_register_value_to_current = [&](uint16_t value) -> double {
+		return interpolate<double>(0x0000, 0x6C00, value, 4, 20);
+	};
+
+	auto convert_current_to_value = [&](double value, double from, double to) -> double {
+		return interpolate<double>(4, 20, value, from, to);
+	};
+
+	auto convert_value_to_current = [&](double value, double from, double to) -> double {
+		return interpolate<double>(from, to, value, 4, 20);
+	};
+
+	// pressure in bar
+	auto pressure_to_register_value = [&](double pressure) -> uint16_t {
+		// convert to current
+		double current = convert_value_to_current(pressure, -1, 25); // assume range -1 to 25 bar
+
+		// convert to register value
+		return interpolate<uint16_t>(4, 20, current, 0x0000, 0x6C00);
+	};
+
+	// flow in l/min
+	auto flow_to_register_value = [&](double flow) -> uint16_t {
+		// convert to current
+		double current = convert_value_to_current(flow, 0, 130*1000/60);
+
+		// convert to register value
+		return interpolate<uint16_t>(4, 20, current, 0x0000, 0x6C00);
+	};
+
+	auto percentage_to_register_value = [&](double percentage) -> uint16_t {
+		// convert to current
+		double current = convert_value_to_current(percentage, 0, 1);
+
+		std::cout << current << " mA - 0x" << std::setw(4) << std::hex << interpolate<uint16_t>(4, 20, current, 0x0000, 0x6C00) << std::endl;
+
+		// convert to register value
+		return interpolate<uint16_t>(4, 20, current, 0x0000, 0x6C00);
+	};
+
 	while(1) {
 		timer.wait_on_tick();
 
 		uint16_t reg[128];
 
-		int num = modbus_read_registers(ctx, ADDR_HOLDING_REGISTER_START, 50, reg);
+		int num;
+		if(!fake) {
+			num = modbus_read_registers(ctx, ADDR_HOLDING_REGISTER_START, 50, reg);
+		} else {
+			reg[19] = pressure_to_register_value(5);
+			reg[20] = pressure_to_register_value(6);
+			reg[21] = flow_to_register_value(15);
+			reg[22] = percentage_to_register_value(0.89);
 
+			reg[43] = temperature_to_register_value(50);
+			reg[44] = temperature_to_register_value(51);
+			reg[45] = temperature_to_register_value(52);
+			reg[46] = temperature_to_register_value(53);
+
+			num = 50;
+		}
+		
 		if(num == -1) {
 			logfile.write(LOG_CRIT, "error reading registers, exiting: %s", modbus_strerror(errno));
 			modbus_close(ctx);
@@ -232,8 +309,19 @@ int main(int argc, char **argv){
 			try {
 				std::string parameter = e["parameter name"];
 				int address_offset = e["address offset"];
+				int conversion = e["conversion"];
 
-				attribute_data[parameter] = getValue(reg, address_offset);
+				uint16_t reg_value = getValue(reg, address_offset);
+				logfile.write(LOG_INFO, "reg_value[%s]: 0x%X (%d)", parameter.c_str(), reg_value, reg_value);
+				double value;
+
+				switch(conversion) {
+					case CONV_CURRENT: value = convert_register_value_to_current(reg_value); break;
+					case CONV_TEMP: value = register_value_to_temperature(reg_value); break;
+					default: value = reg_value;
+				}
+
+				attribute_data[parameter] = value;
 			} catch(const std::exception& e) {
 				logfile.write(LOG_ERR, "error getting value: %s", e.what());
 			}
